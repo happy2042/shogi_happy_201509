cd6cfb5ddabf22630c56902addc575cadf9ced3d
aimosaka04 <aimosaka04@gmail.com>
2015-09-10 15:55:50 +0900

    UserManagerをシーンをまたいでも保持されるよう変更

--- Assets/Scripts/UserManager.cs
+++ Assets/Scripts/UserManager.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 
 public class UserManager : MonoBehaviour {
 
-	 public Dictionary<string, object> userInfo;
+	public Dictionary<string, object> userInfo;
+
+	void Awake(){
+		DontDestroyOnLoad(this.gameObject);
+	}
 
 	// Use this for initialization
 	void Start () {
