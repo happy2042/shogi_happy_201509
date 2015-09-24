3f7a9ff479eb658074061c40661d9394b487ea66
aimosaka04 <aimosaka04@gmail.com>
2015-09-24 20:01:09 +0900

    駒の移動実装、マーカーの位置に移動するだけ

---
+++ Assets/Scripts/KomaMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KomaMove : MonoBehaviour {
+
+	public GameObject activeMove;
+
+	// Use this for initialization
+	void Start () {
+		activeMove = (GameObject)Resources.Load ("Prefabs/ActiveMove");
+	}
+
+	public void OnClick(){
+		MoveSet ();
+	}
+
+	void MoveSet(){
+		GameObject movePrefab = (GameObject)Instantiate(activeMove);
+		movePrefab.transform.SetParent (this.transform, false);
+	}
+}
